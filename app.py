3b6e36ae25b2d9abe2fafc84ee50275a696ae343
mrfakename <mrfakename@users.noreply.huggingface.co>
2024-01-27 23:16:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ def sync_db():
         token=os.getenv('HF_TOKEN')
     )
     while True:
-        time.sleep(60 * 15)
+        time.sleep(45)
         print("Uploading DB")
         api.upload_file(
             path_or_fileobj='database.db',
